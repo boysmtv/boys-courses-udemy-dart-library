7c1a534f654e7c82effa8dba312b5bdb1ebc6a9e
boysmtv <boys.mtv@gmail.com>
2023-09-25 17:54:13 +0700

    update say hello

--- lib/src/say_hello.dart
+++ lib/src/say_hello.dart
@@ -2,6 +2,10 @@ String sayHello(String name) {
   return "Hello $name, Nice To See You!";
 }
 
+String sayBeb(String name) {
+  return "Beb $name, Nice To See You!";
+}
+
 void sayHi() {
   print('Hi');
 }
