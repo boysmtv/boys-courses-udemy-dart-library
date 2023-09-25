3fbb0b8cc57803f547fffe16e1939b2acfbabc38
boysmtv <boys.mtv@gmail.com>
2023-09-25 14:28:36 +0700

    initial dart library

---
+++ example/hello.dart
@@ -0,0 +1,10 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayHello("Dedy"));
+  // print(sayHi("Dedy")); not showing
+
+  var customer = Customer();
+  var product = Product();
+  // var category = Category(); not showing
+}
